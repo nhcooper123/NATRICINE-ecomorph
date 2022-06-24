c237c9940b9adb307abacbd997eca1d742a14458
Natalie Cooper <ncooper@tcd.ie>
2022-06-24 13:59:38 +0100

    Modify code to use LSR instead of residuals

--- analyses/Linear/01-extract-species-means.R
+++ analyses/Linear/01-extract-species-means.R
@@ -1,6 +1,7 @@
 # Title: Head shape variation in natricine snakes correlates with habit and diet
 # V. Deepak & Natalie Cooper, 2021 
 # Extract species means for natricine linear morphology dataset
+# Modified 2022
 
 #---------------------------
 # Load libraries
@@ -15,35 +16,31 @@ library(tidyverse)
 ds <- read_csv("data/Linear/metadata-data-natricine-LM.csv")
 
 #----------------------------------------------
-# Get log10 values of variables
-# Regress against SVL to reduce size influence 
-# and focus on shape, and extract residuals
-# Take species means of residuals
+# Get log10 shape ratios (LSR) for each specimen
+# Take species means of LSR
 #----------------------------------------------
 snake <- 
   ds %>%
-  # Take all variables from JL to TL and log transform
-  # But exclude DSR
-  # This creates the new variables JL_log10 to TL_log10
+  # Next get the geometric mean of variables JL:EE only, as these
+  # are the variables used in the PCA
+  # need to use rowwise as we want geometric means per row
+  rowwise() %>% 
+  mutate(geo_mean = geometric.mean(c(JL, HLRA, HLV, HWLSL, HWFSL, HW, 
+                                     HHP, HH, ES, EN, ED, NL, EL, NN, EE))) %>%
+  # Now get log shape ratios by dividing each variable JL:EE by the geometric mean,
+  # and logging the ratio.
   # vars tells it what variables to use
-  # inside the list is the function, and what we'd like it to call the new variables
-  mutate_at(vars(JL:TL, -DSR), list(log10 = log10)) %>%
-  # Now get residuals from regressions for each _log10 variable (excluding SVL itself)
-  # And also DSR which we did not log10 transform
-  # again vars tells it what variables to use
-  # list contains the function, and we will call the outputs _res
+  # list contains the function, and we will call the outputs _LSR (log shape ratio)
   # the ~ tells it we are going to use a function, and will replace the . with each
   # variable in turn.
-  mutate_at(vars(JL_log10:TL_log10, DSR, -SVL_log10), 
-            list(res = ~residuals(lm(. ~ SVL_log10, data = ds, 
-                                     na.action = na.exclude)))) %>%
+  mutate_at(vars(JL:EE), list(LSR = ~ log10( . / geo_mean))) %>%
   # Finally let's get the species means using summarise
   # we will group by the other variables we want to keep in the dataset
-  group_by(Species,Habit,Diet, Origin, Lineage) %>%
+  group_by(Species, Habit, Diet, Origin, Lineage) %>%
   # Then get the means of the residuals columns
-  summarise_at(vars(JL_log10_res:DSR_res), mean, na.rm = TRUE)
+  summarise_at(vars(JL_LSR:EE_LSR), mean, na.rm = TRUE)
 
 #----------------------------------
 # Export the file (using write_csv)
 #----------------------------------
-# write_csv(snake, file = "data/Linear/linear-species-means.csv")
+# write_csv(snake, file = "data/Linear/linear-species-means-LSR.csv")
